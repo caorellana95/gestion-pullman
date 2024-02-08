5e2bf1e44af4ca3fe267bb0007e3d8d40049be8d
unknown <caorellana95@gmail.com>
2024-02-08 10:57:41 -0300

    init

---
+++ pages/_document.tsx
@@ -0,0 +1,13 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head />
+      <body className='bg-[#003FA2]'>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
