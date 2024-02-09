a4670c6c79bb67a80b3b9346bb92acba419dfab5
unknown <caorellana95@gmail.com>
2024-02-09 16:50:33 -0300

    cambio

--- pages/cambiar.tsx
+++ pages/cambiar.tsx
@@ -1,5 +1,53 @@
+import Image from "next/image"
+
 export default function Cambiarr(){
     return (
-        <div></div>
+        <div>
+            <div className=" flex mt-8 justify-around">
+                <Image
+                    src={"/images/arrow-left.png"}
+                    alt="Logo"
+                    width={20}
+                    height={19}
+                    className="  "
+                />  
+                
+                <h1 className="text-lg">Cambia de <b>pasaje</b></h1>
+                <Image
+                    src={"/images/house.png"}
+                    alt="Logo"
+                    width={20}
+                    height={19}
+                    className="  "
+                />
+            </div>
+            <div className="bg-white  mt-8 rounded-md border-2 shadow mx-12 ">
+                <div className="flex mt-8 px-auto">
+                <div className="mb-12">
+                <label className="font-bold">Número de reserva</label>
+                <input type="text" 
+                       className="rounded-md border-black  border-2 h-10 w-28 mx-2 "
+                       placeholder="PMXXXXXXXX" />
+
+
+                </div>
+                <div className="">
+                <label className="font-bold">Fechs de reserva</label>
+                <input type="text"
+                       className="rounded-md border-black  border-2 h-10  w-28 mx-2"
+                       placeholder="XX-XX-XXXX" />
+
+                </div>
+                </div>
+                <div className="mb-8">
+                <label className="font-bold">Correo electrónico</label>
+                <input type="text"
+                       className="rounded-md border-black  border-2 h-10  w-48 mx-2"
+                       placeholder="Correo electrónico" />
+
+                </div>
+
+            </div>
+        </div>
     )
 }
--- pages/menu.tsx
+++ pages/menu.tsx
@@ -13,108 +13,108 @@ const Menu: React.FC<MenuProps> = ({ menuOpen, menu }) => {
         <div className="px-4 py-6">
             <div className='flex'>
                 <Image
-                    src={"/images/plus.png"}
-                    alt="Logo"
-                    width={20}
-                    height={19}
-                    className=" ml-10 mt-12 absolute"
+                  src={"/images/plus.png"}
+                  alt="Logo"
+                  width={20}
+                  height={19}
+                  className=" ml-10 mt-12 absolute"
                  />
                 <Image
-                    src={"/images/camera.png"}
-                    alt="Logo"
-                    width={70}
-                    height={19}
-                    className=" mt-2 "
+                  src={"/images/camera.png"}
+                  alt="Logo"
+                  width={70}
+                  height={19}
+                  className=" mt-2 "
                  />
             <h2 className="text-gray-400 text-lg mt-8">¡Hola!</h2>
 
             </div>
           <ul className="text-gray-400 mt-4 ml-4">
                 <Image
-                    src={"/images/smartphone.png"}
-                    alt="Logo"
-                    width={35}
-                    height={19}
-                    className=" mr-2 absolute"
+                  src={"/images/smartphone.png"}
+                  alt="Logo"
+                  width={35}
+                  height={19}
+                  className=" mr-2 absolute"
                  />
             <li className="mb-4 ml-8">Recargar PullmanPay</li>
-            <Image
-                    src={"/images/smartphone.png"}
-                    alt="Logo"
-                    width={35}
-                    height={19}
-                    className=" mr-2 absolute"
+                <Image
+                   src={"/images/smartphone.png"}
+                   alt="Logo"
+                   width={35}
+                   height={19}
+                   className=" mr-2 absolute"
                  />
-                 <Image
-                    src={"/images/exchange lr.png"}
-                    alt="Logo"
-                    width={12}
-                    height={19}
-                    className=" ml-3 mt-3 absolute"
+                <Image
+                   src={"/images/exchange lr.png"}
+                   alt="Logo"
+                   width={12}
+                   height={19}
+                   className=" ml-3 mt-3 absolute"
                  />
             <li className="mb-4 ml-8">Compartir Saldo</li>
-            <Image
-                    src={"/images/profile.png"}
-                    alt="Logo"
-                    width={20}
-                    height={19}
-                    className=" ml-2 mt-1 absolute"
+                <Image
+                   src={"/images/profile.png"}
+                   alt="Logo"
+                   width={20}
+                   height={19}
+                   className=" ml-2 mt-1 absolute"
                  />
             <li className="mb-4 ml-8">Mi cuenta</li>
-            <Image
-                    src={"/images/boleto.png"}
-                    alt="Logo"
-                    width={25}
-                    height={19}
-                    className=" mr-2 ml-1 absolute"
+                <Image
+                   src={"/images/boleto.png"}
+                   alt="Logo"
+                   width={25}
+                   height={19}
+                   className=" mr-2 ml-1 absolute"
                  />
             <li className="mb-4 ml-8">Mis viajes</li>
-            <Image
-                    src={"/images/statistics.png"}
-                    alt="Logo"
-                    width={30}
-                    height={19}
-                    className=" mr-2 absolute"
+                <Image
+                   src={"/images/statistics.png"}
+                   alt="Logo"
+                   width={30}
+                   height={19}
+                   className=" mr-2 absolute"
                  />
             <li className="mb-4 ml-8">Mis movimientos</li>
-            <Image
-                    src={"/images/coupon.png"}
-                    alt="Logo"
-                    width={35}
-                    height={19}
-                    className=" mr-2 mt-1 absolute"
+                <Image
+                  src={"/images/coupon.png"}
+                  alt="Logo"
+                  width={35}
+                  height={19}
+                  className=" mr-2 mt-1 absolute"
                  />
             <li className="mb-4 ml-8">Promociones</li>
-            <Image
-                    src={"/images/chat.png"}
-                    alt="Logo"
-                    width={30}
-                    height={19}
-                    className=" mr-2 mb-1 absolute"
+                <Image
+                  src={"/images/chat.png"}
+                  alt="Logo"
+                  width={30}
+                  height={19}
+                  className=" mr-2 mb-1 absolute"
                  />
             <li className="mb-4 ml-8">Comparte y gana</li>
-            <Image
-                    src={"/images/info.png"}
-                    alt="Logo"
-                    width={20}
-                    height={19}
-                    className=" mr-2 ml-2 absolute"
+                <Image
+                  src={"/images/info.png"}
+                  alt="Logo"
+                  width={20}
+                  height={19}
+                  className=" mr-2 ml-2 absolute"
                  />
             <li className="mb-4 ml-8">Términos de uso</li>
-            <Image
-                    src={"/images/politics.png"}
-                    alt="Logo"
-                    width={25}
-                    height={19}
-                    className=" mr-2 absolute"
+                <Image
+                  src={"/images/politics.png"}
+                  alt="Logo"
+                  width={25}
+                  height={19}
+                  className=" mr-2 absolute"
                  />
             <li className="mb-4 ml-8">Políticas de privacidad</li>
-            <Image
-                    src={"/images/headphone.png"}
-                    alt="Logo"
-                    width={30}
-                    height={19}
-                    className=" mr-2 absolute"
+                <Image
+                  src={"/images/headphone.png"}
+                  alt="Logo"
+                  width={30}
+                  height={19}
+                  className=" mr-2 absolute"
                  />
             <li className="mb-2 ml-8">Contáctanos</li>
           </ul>
